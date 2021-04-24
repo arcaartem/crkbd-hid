063b5cc2efc3b9824688d066b07731ee44830020
Arca <arca.artem@gmail.com>
2021-04-24 11:43:44 +0100

    move system information to own class

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
 import CorneKeyboard from './corne-keyboard.js';
-import si from 'systeminformation';
+import SystemInformation from './system-information.js';
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-const { currentLoad } = await si.currentLoad();
-const { active, total } = await si.mem();
-const { main } = await si.cpuTemperature();
-const defaultInterface = await si.networkInterfaceDefault();
-
-await si.networkStats(defaultInterface);
-await sleep(500);
-const networks = await si.networkStats(defaultInterface);
-
-const { rx_sec, tx_sec } = networks[0];
+var systemInformation = new SystemInformation();
+var stats = await systemInformation.getStats();
+var { load, memoryUtilisation, temperature, receivedBytesPerSec, sentBytesPerSec } = stats;
 
 var keyboard = new CorneKeyboard();
 keyboard.open();
 try {
-    keyboard.sendText(0, `C:${currentLoad.toFixed(1)}%  M:${(active/total*100).toFixed(1)}%`);
-    keyboard.sendText(1, `T:${main}C  N:\x18${(rx_sec/1024).toFixed(0)}k/\x19${(tx_sec/1024).toFixed(0)}k`);
+    keyboard.sendText(0, `C:${load.toFixed(1)}%  M:${memoryUtilisation.toFixed(1)}%`);
+    keyboard.sendText(1, `T:${temperature}C  N:\x18${(receivedBytesPerSec/1024).toFixed(0)}k/\x19${(sentBytesPerSec/1024).toFixed(0)}k`);
 } finally {
     keyboard.close();
 }
---
+++ system-information.js
@@ -0,0 +1,29 @@
+import si from 'systeminformation';
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+class SystemInformation {
+    async getStats() {
+        const { currentLoad } = await si.currentLoad();
+        const { active, total } = await si.mem();
+        const { main } = await si.cpuTemperature();
+        const defaultInterface = await si.networkInterfaceDefault();
+
+        await si.networkStats(defaultInterface);
+        await sleep(500);
+        const networks = await si.networkStats(defaultInterface);
+
+        const { rx_sec, tx_sec } = networks[0];
+        return {
+            load: currentLoad,
+            memoryUtilisation: active/total*100,
+            temperature: main,
+            receivedBytesPerSec: rx_sec,
+            sentBytesPerSec: tx_sec
+        };
+    }
+}
+
+export default SystemInformation
