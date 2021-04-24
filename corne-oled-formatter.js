ed5f3f1a5caa6a85646f5ef7d9281d46ea63fe2b
Arca <arca.artem@gmail.com>
2021-04-24 13:02:34 +0100

    make network rates scalable

--- corne-oled-formatter.js
+++ corne-oled-formatter.js
@@ -1,12 +1,37 @@
-const UP_ARROW = `\x18`;
-const DOWN_ARROW = `\x19`;
+const DEBUG = false;
+
+let UP_ARROW = '\x18';
+let DOWN_ARROW = '\x19';
+
+if (DEBUG) {
+    UP_ARROW = 'U';
+    DOWN_ARROW = 'D';
+}
+
+const BYTE_SCALE_CHART = [ '', 'K', 'M', 'G', 'T', 'P' ];
+
+function scaleBytesPerSec(bytesPerSec) {
+    let scale = Math.floor(Math.log10(bytesPerSec + 1)/3);
+    let divisor = Math.pow(10, (scale) * 3);
+    return `${(bytesPerSec/divisor).toFixed(0)}${BYTE_SCALE_CHART[scale]}`;
+}
 
 class CorneOledFormatter {
     formatSystemInformation(systemInformation) {
         const { load, memoryUtilisation, temperature, receivedBytesPerSec, sentBytesPerSec } = systemInformation;
+
+        const cpuText = load.toFixed(0).toString().padStart(4);
+        const memoryText = memoryUtilisation.toFixed(0).toString().padStart(4);
+        const temperatureText = temperature.toFixed(0).toString().padStart(4);
+        const receivedBytesText = `${scaleBytesPerSec(receivedBytesPerSec)}`.padStart(4);
+        const sentBytesText = `${scaleBytesPerSec(sentBytesPerSec)}`.padStart(4);
+
         const oledLines = [];
-        oledLines.push(`C:${load.toFixed(1)}%  M:${memoryUtilisation.toFixed(1)}%`);
-        oledLines.push(`T:${temperature}C  N:${UP_ARROW}${(receivedBytesPerSec/1024).toFixed(0)}k/${DOWN_ARROW}${(sentBytesPerSec/1024).toFixed(0)}k`);
+        oledLines.push(`C:${cpuText}% M:${memoryText}%`);
+        oledLines.push(`T:${temperatureText}C N:${receivedBytesText}${DOWN_ARROW}/${sentBytesText}${UP_ARROW}`);
+        if (DEBUG) {
+            console.log(oledLines, oledLines.map(l => l.length));
+        }
         return oledLines
     }
 }
